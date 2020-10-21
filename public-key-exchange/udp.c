f57cce1c5d748c4dbbd95746cd1a8ebfd4936227
MilaniStef <stefano.milani96@gmail.com>
2020-10-21 17:41:40 +0200

    Fix code formatting and file organization
    Fixed a really bad code formatting and files organization.
    In udp.c the function handle_message now invoke other function according
    to the message type. Fixed a poor management of server_buffer when
    receiving new message. utils.h and utils.c substituted by key-management
    files.
    
    Back to micro-ecc to manage public/private keys of the device. The
    library is more complete and mainteined wrt tinycrypt_ecc.
    
    Signed-Off-By: Stefano Milani <stefano.milani96@gmail.com>

--- public-key-exchange/udp.c
+++ public-key-exchange/udp.c
@@ -22,19 +22,315 @@
  *
  */
 
+/* needed for posix usleep */
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+#ifndef DEFAULT_NUM
+#define DEFAULT_NUM 1
+#endif
+
+#ifndef DEFAULT_DELAY
+#define DEFAULT_DELAY 1000000
+#endif
+
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include "thread.h"
+#include "key-management.h"
+#include "tinycrypt/aes.h"
+#include "tinycrypt/constants.h"
+
 #include "udp.h"
-#include "utils.h"
+
+// Server utils define
+#define SERVER_MSG_QUEUE_SIZE   8
+#define SERVER_BUFFER_SIZE      128
+
+// Message utils define 
+#define MSG_TYPE_SIZE   		1
+#define KEY_REQUEST     		0x01
+#define KEY_ACK         		0x02
+#define ACK             		0x03
+#define MSG             		0x04
+
 // Server global variables
 static int server_socket=-1;
 static char server_buffer[SERVER_BUFFER_SIZE];
 static char server_stack[THREAD_STACKSIZE_DEFAULT];
 static msg_t server_msg_queue[SERVER_MSG_QUEUE_SIZE];
+
 // Keys management global variables
 const struct uECC_Curve_t *curve;
 size_t curve_size;
 size_t public_key_size;
 Key *key;
 Device* dev=NULL;
+
+// Function declarations
+int udp_send(char *addr_str, char *port_str, char *data, size_t data_len, unsigned int num, unsigned int delay);
+int handle_message(char* src_addr);
+int manage_key_request(char* src_addr);
+int decrypt_received_message(char* src_addr);
+int store_dev_info(char* addr, uint8_t* compr);
+void *_server_thread(void *args);
+int send_ack(char* src_addr);
+
+// Send UDP packet
+int udp_send(char *addr_str, char *port_str, char *data, size_t data_len,  unsigned int num,
+                    unsigned int delay) {
+    struct sockaddr_in6 src, dst;
+    uint16_t port;
+    int s;
+    src.sin6_family = AF_INET6;
+    dst.sin6_family = AF_INET6;
+    memset(&src.sin6_addr, 0, sizeof(src.sin6_addr));
+    /* parse destination address */
+    if (inet_pton(AF_INET6, addr_str, &dst.sin6_addr) != 1) {
+        puts("Error: unable to parse destination address");
+        return 1;
+    }
+    /* parse port */
+    port = atoi(port_str);
+    dst.sin6_port = htons(port);
+    src.sin6_port = htons(port);
+    s = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    if (s < 0) {
+        puts("error initializing socket");
+        return 1;
+    }
+    for (unsigned int i = 0; i < num; i++) {
+        if (sendto(s, data, data_len, 0, (struct sockaddr *)&dst, sizeof(dst)) < 0) {
+            puts("could not send");
+        }
+        else {
+            printf("Success: send %u byte to %s:%u\n", (unsigned)data_len, addr_str, port);
+        }
+
+        usleep(delay);
+    }
+    close(s);
+    return 0;
+}
+
+// Handle received message
+int handle_message(char* src_addr) {
+    uint8_t msg_type;
+    // Get message type from message (First char)
+    memcpy(&msg_type, server_buffer, MSG_TYPE_SIZE);
+	// Behave differently according to the message type
+    switch (msg_type) {
+        case KEY_REQUEST:
+			// Public key exchange request received
+			if(manage_key_request(src_addr) == -1) {
+				perror("Failed to send back public key");
+				return -1;
+			}
+            break;
+        case KEY_ACK:
+			// Public key received, Send back ACK
+			if(send_ack(src_addr) == -1) {
+				perror("Failed to send back ACK message");
+				return -1;
+			}
+            break;
+        case ACK:
+			// ACK received, Handshake completed
+            printf("\n-ACK RECEIVED... your public key has been correctly received.\n");
+            break;
+        case MSG:
+			// Ecrypted message received
+			if(decrypt_received_message(src_addr) == -1) {
+				perror("Failed to decrypt received message");
+				return -1;
+			}
+            break;
+        default:
+            printf("\n-MESSAGE TYPE NOT RECOGNIZED!\n");
+    }
+    return 0;
+}
+
+// Manage the request of your public key 
+int manage_key_request(char* src_addr) {
+	uint8_t* src_pub_key = (uint8_t*) malloc(public_key_size);
+    
+	// Get public key from message
+	memcpy(src_pub_key, server_buffer+MSG_TYPE_SIZE, public_key_size);
+    printf("\n-PUBLIC KEY REQUEST FROM %s\n", src_addr);
+    
+	// Print received public key
+	printf("Public key received:");
+    print_key(src_pub_key, public_key_size);
+    
+	// Store info of struct
+    if(store_dev_info(src_addr, src_pub_key) == -1) {
+		perror("Failed to store device public key");
+		return -1;
+	}
+    
+	// Clean server buffer for response
+    memset(server_buffer, 0, SERVER_BUFFER_SIZE);
+    
+	// Fill server_buffer with your key
+	memset(server_buffer, KEY_ACK, MSG_TYPE_SIZE);
+    memcpy(server_buffer+MSG_TYPE_SIZE, key->pub, public_key_size);
+    
+	// Send your public key
+    printf("\n-REPLAYING TO %s WITH MY COMPRESSED KEY...\n", src_addr);
+    udp_send(src_addr, DEFAULT_PORT, server_buffer, public_key_size+MSG_TYPE_SIZE,  DEFAULT_NUM, DEFAULT_DELAY);
+    printf("KEY SENT\n");
+
+	return 0;
+}
+
+// Send back ACK message to conclude handshake
+int send_ack(char* src_addr) {
+	uint8_t* src_pub_key = (uint8_t*) malloc(public_key_size);
+    
+	// Get public key from message
+	memcpy(src_pub_key, server_buffer+MSG_TYPE_SIZE, public_key_size);
+    printf("\n-RECEIVED PUBLIC KEY FROM %s PREVIOUSLY REQUESTED\n", src_addr);
+    printf("Public key received:");
+    print_key(src_pub_key, public_key_size);
+    
+	// Store info on struct
+    if(store_dev_info(src_addr, src_pub_key) == -1) {
+		perror("Failed to store device public key");
+		return -1;
+	}
+    
+	// Clean the server_buffer for response
+    memset(server_buffer, 0, SERVER_BUFFER_SIZE);
+    
+	// Fill the buffer with ACK
+	memset(server_buffer, ACK, MSG_TYPE_SIZE);
+    
+	// Send ACK
+    printf("\n-SENDING ACK TO %s.\n", src_addr);
+    udp_send(src_addr, DEFAULT_PORT, server_buffer, MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
+    printf("ACK SENT\n");
+
+	return 0;
+}
+
+// Encrypted message received
+int decrypt_received_message(char* src_addr) {
+    printf("\n-Decrypting message!\n");
+    // Initializing aes struct
+    struct tc_aes_key_sched_struct s;
+    // Set key
+    if(tc_aes128_set_decrypt_key(&s, (const uint8_t *)dev->secret) == TC_CRYPTO_FAIL) {
+		perror("Failed to set aes decrypt key");
+		return -1;
+	} 
+    // Decrypt
+    uint8_t msg[TC_AES_BLOCK_SIZE + 1];
+    if(tc_aes_decrypt(msg, (const uint8_t*) server_buffer+MSG_TYPE_SIZE, &s) == TC_CRYPTO_FAIL) {
+		perror("Failed to decrypt message");
+		return -1;
+	}
+    printf("Message received: \"%s\"\tfrom: %s\n", msg, src_addr);
+	
+	return 0;
+}
+
+// Store IP address and Public key of a device 
+int store_dev_info(char* addr, uint8_t* pub_key){
+    dev = (Device*) malloc(sizeof(struct device_t));
+    // Get IPv6 address
+    dev->ip_addr = (char*) malloc(sizeof(char)*INET6_ADDRSTRLEN);
+    memcpy(dev->ip_addr, addr, INET6_ADDRSTRLEN);
+    // Get pub key
+    dev->pub_key = (uint8_t*) malloc(sizeof(uint8_t)*public_key_size);
+    memcpy(dev->pub_key, pub_key, public_key_size);
+    if(!uECC_valid_public_key(dev->pub_key, curve)){
+        printf("INVALID PUB KEY!!\n");
+        return -1;
+    }
+    // Generate and store secret
+    dev->secret = (uint8_t*) malloc(sizeof(uint8_t)*curve_size);
+    if(!(uECC_shared_secret(dev->pub_key, key->priv, dev->secret, curve)))
+        return -1;
+    // Print info stored in the struct
+    printf("\n\n TESTING THE STORING FUNCTION\n");
+    printf("IP Address: %s\n", dev->ip_addr);
+    printf("Public key:\n");
+    print_key(dev->pub_key, public_key_size);
+    printf("Shared secret: \n");
+    print_key(dev->secret, curve_size);
+    printf("\n\n END \n");
+    return 0;
+}
+
+/*
+ *	Shell commands
+ */
+
+// Shell command to start key exchange
+int start_exchange(int argc, char **argv){
+ 
+    if(argc < 2){
+        printf("usage: start_exchange <IPv6 address>\n");
+        return -1;
+    }    
+    // Get dest IP address
+    printf("\nSENDING PUBLIC KEY EXCHANGE REQUEST TO %s\n", argv[1]);
+    // Prapare message to send
+    char buf[public_key_size+MSG_TYPE_SIZE];
+    memset(buf, 0, public_key_size+MSG_TYPE_SIZE);
+	memset(buf, KEY_REQUEST, MSG_TYPE_SIZE);
+    memcpy(buf+MSG_TYPE_SIZE, key->pub, public_key_size);
+    udp_send(argv[1], DEFAULT_PORT, buf, public_key_size+MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
+    return 0;
+}
+
+// Shell command to send encrypted message
+int send_encrypted(int argc, char **argv){
+    
+    if(argc < 3) {
+        printf("usage: send_encrypted <IPv6 address> <msg>\n");
+        return -1;
+    }
+    if(dev == NULL) {
+        printf("Exchange pub key with a device first!\n");
+        return -1;
+    }
+    printf("IP addr in argument: %s\n",argv[1]);
+    printf("IP addr stored: %s\n", dev->ip_addr);
+    if(memcmp(dev->ip_addr, argv[1], 25) != 0) {
+        printf("Key exchange not yet performed with %s\n", argv[1]);
+        return -1;
+    }
+    // Initialize aes struct
+    struct tc_aes_key_sched_struct s;
+
+    // Initialize key
+    tc_aes128_set_encrypt_key(&s, dev->secret);
+
+    // Encrypt data
+    uint8_t cipher[TC_AES_BLOCK_SIZE + 1];
+    tc_aes_encrypt(cipher, (const uint8_t *)argv[2], &s);
+
+    // Forge server buffer
+	memset(server_buffer, MSG, MSG_TYPE_SIZE);
+    memcpy(server_buffer+MSG_TYPE_SIZE, cipher, TC_AES_BLOCK_SIZE + 1);
+    
+    // Send encrypted message
+    udp_send(argv[1], DEFAULT_PORT, server_buffer, TC_AES_BLOCK_SIZE + 1 + MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
+
+    return 0;
+}
+
+/*
+ *	Start server functions
+ */
 
 // Start server
 void *_server_thread(void *args) {
@@ -89,136 +385,9 @@ void *_server_thread(void *args) {
     return NULL;
 }
 
-int store_dev_info(char* addr, uint8_t* pub_key){
-    dev = (Device*) malloc(sizeof(struct device_t));
-    // Get IPv6 address
-    dev->ip_addr = (char*) malloc(sizeof(char)*INET6_ADDRSTRLEN);
-    memcpy(dev->ip_addr, addr, INET6_ADDRSTRLEN);
-    // Get pub key
-    dev->pub_key = (uint8_t*) malloc(sizeof(uint8_t)*public_key_size);
-    memcpy(dev->pub_key, pub_key, public_key_size);
-    if(uECC_valid_public_key(dev->pub_key, curve)){
-        printf("INVALID PUB KEY!!\n");
-        return 0;
-    }
-    // Generate and store secret
-    dev->secret = (uint8_t*) malloc(sizeof(uint8_t)*curve_size);
-    if(!(uECC_shared_secret(dev->pub_key, key->priv, dev->secret, curve)))
-        return 0;
-    // Print info stored in the struct
-    printf("\n\n TESTING THE STORING FUNCTION\n");
-    printf("IP Address: %s\n", dev->ip_addr);
-    printf("Public key:\n");
-    print_key(dev->pub_key, public_key_size);
-    printf("Shared secret: \n");
-    print_key(dev->secret, curve_size);
-    printf("\n\n END \n");
-    return 1;
-}
-
-// Handle received message
-int handle_message(char* src_addr) {
-    char str_msg_type;
-    uint8_t msg_type;
-    uint8_t* src_pub = (uint8_t*) malloc(sizeof(uint8_t)*(public_key_size));
-    // Get message type from message (First char)
-    memcpy(&str_msg_type, server_buffer, MSG_TYPE_SIZE);
-    msg_type = atoi(&str_msg_type);
-    // Get compressed key from message
-    for(size_t i = 0 ; i < public_key_size ; i++) {
-        src_pub[i] = (uint8_t) server_buffer[MSG_TYPE_SIZE + i];
-    }
-    switch (msg_type) {
-        case KEY_REQUEST:
-            printf("\n-PUBLIC KEY REQUEST FROM %s\n", src_addr);
-            printf("Public key received:");
-            print_key(src_pub, public_key_size);
-            // Store info of struct
-            store_dev_info(src_addr, src_pub);
-            // Clean server buffer for response
-            memset(server_buffer, 0, SERVER_BUFFER_SIZE);
-            // Fill server_buffer with your key
-            sprintf(server_buffer, "%u", (char)KEY_ACK);
-            memcpy(server_buffer+MSG_TYPE_SIZE, key->pub, public_key_size);
-            // Send your public key
-            printf("\n-REPLAYING TO %s WITH MY COMPRESSED KEY...\n", src_addr);
-            udp_send(src_addr, DEFAULT_PORT, server_buffer, public_key_size+MSG_TYPE_SIZE,  DEFAULT_NUM, DEFAULT_DELAY);
-            printf("KEY SENT\n");
-            break;
-        case KEY_ACK:
-            printf("\n-RECEIVED PUBLIC KEY FROM %s PREVIOUSLY REQUESTED\n", src_addr);
-            printf("Public key received:");
-            print_key(src_pub, public_key_size);
-            // Store info on struct
-            store_dev_info(src_addr, src_pub);
-            // Clean the server_buffer for response
-            memset(server_buffer, 0, SERVER_BUFFER_SIZE);
-            // Fill the buffer with ACK
-            sprintf(server_buffer, "%u", (char)ACK);
-            // Send ACK
-            printf("\n-SENDING ACK TO %s.\n", src_addr);
-            udp_send(src_addr, DEFAULT_PORT, server_buffer, MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
-            printf("ACK SENT\n");
-            break;
-        case ACK:
-            printf("\n-ACK RECEIVED... your public key has been correctly received.\n");
-            break;
-        case MSG:
-            printf("\n-Decrypting message!\n");
-            // Initializing aes struct
-            struct tc_aes_key_sched_struct s;
-            // Set key
-            tc_aes128_set_decrypt_key(&s, (const uint8_t *)dev->secret);
-            // Decrypt
-            uint8_t msg[TC_AES_BLOCK_SIZE + 1];
-            tc_aes_decrypt(msg, src_pub, &s);
-            printf("Message received: %s\tfrom: %s\n", msg, src_addr);
-            break;
-        default:
-            printf("\n-MESSAGE TYPE NOT RECOGNIZED!\n");
-    }
-    return 0;
-}
-
-// Send UDP packet
-int udp_send(char *addr_str, char *port_str, char *data, size_t data_len,  unsigned int num,
-                    unsigned int delay) {
-    struct sockaddr_in6 src, dst;
-    uint16_t port;
-    int s;
-    src.sin6_family = AF_INET6;
-    dst.sin6_family = AF_INET6;
-    memset(&src.sin6_addr, 0, sizeof(src.sin6_addr));
-    /* parse destination address */
-    if (inet_pton(AF_INET6, addr_str, &dst.sin6_addr) != 1) {
-        puts("Error: unable to parse destination address");
-        return 1;
-    }
-    /* parse port */
-    port = atoi(port_str);
-    dst.sin6_port = htons(port);
-    src.sin6_port = htons(port);
-    s = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
-    if (s < 0) {
-        puts("error initializing socket");
-        return 1;
-    }
-    for (unsigned int i = 0; i < num; i++) {
-        if (sendto(s, data, data_len, 0, (struct sockaddr *)&dst, sizeof(dst)) < 0) {
-            puts("could not send");
-        }
-        else {
-            printf("Success: send %u byte to %s:%u\n", (unsigned)data_len, addr_str, port);
-        }
-
-        usleep(delay);
-    }
-    close(s);
-    return 0;
-}
-
 // Start UDP server
 int udp_start_server(char *port_str) {
+	int ret = 0;
 
     // Check if server is already running
     if (server_socket >= 0) {
@@ -240,8 +409,11 @@ int udp_start_server(char *port_str) {
     key->pub = (uint8_t*) malloc(sizeof(uint8_t)*public_key_size);
     
     // Generate the keys (private, public, public compressed)
-    generate_keys(key, curve);
-    
+    ret = generate_keys(key, curve);
+    if(ret == -1) {
+		perror("Failed to generate keys");
+		return 1;
+	}
     /* start server (which means registering pktdump for the chosen port) */
     if (thread_create(server_stack, sizeof(server_stack), THREAD_PRIORITY_MAIN - 1,
                       THREAD_CREATE_STACKTEST,
@@ -250,60 +422,5 @@ int udp_start_server(char *port_str) {
         puts("error initializing thread");
         return 1;
     }
-    return 0;
-}
-
-// Shell command to start key exchange
-int start_exchange(int argc, char **argv){
- 
-    if(argc < 2){
-        printf("usage: start_exchange <IPv6 address>\n");
-        return -1;
-    }    
-    // Get dest IP address
-    printf("\nSENDING PUBLIC KEY EXCHANGE REQUEST TO %s\n", argv[1]);
-    // Prapare message to send
-    char buf[public_key_size+MSG_TYPE_SIZE];
-    memset(buf, 0, public_key_size+MSG_TYPE_SIZE);
-    sprintf(buf, "%u", (uint8_t)KEY_REQUEST);
-    memcpy(buf+MSG_TYPE_SIZE, key->pub, public_key_size);
-    udp_send(argv[1], DEFAULT_PORT, buf, public_key_size+MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
-    return 0;
-}
-
-// Shell command to send encrypted message
-int send_encrypted(int argc, char **argv){
-    
-    if(argc < 3) {
-        printf("usage: send_encrypted <IPv6 address> <msg>\n");
-        return -1;
-    }
-    if(dev == NULL) {
-        printf("Exchange pub key with a device first!\n");
-        return -1;
-    }
-    printf("IP addr in argument: %s\n",argv[1]);
-    printf("IP addr stored: %s\n", dev->ip_addr);
-    if(memcmp(dev->ip_addr, argv[1], 25) != 0) {
-        printf("Key exchange not yet performed with %s\n", argv[1]);
-        return -1;
-    }
-    // Initialize aes struct
-    struct tc_aes_key_sched_struct s;
-
-    // Initialize key
-    tc_aes128_set_encrypt_key(&s, dev->secret);
-
-    // Encrypt data
-    uint8_t cipher[TC_AES_BLOCK_SIZE + 1];
-    tc_aes_encrypt(cipher, (const uint8_t *)argv[2], &s);
-
-    // Forge server buffer
-    sprintf(server_buffer, "%u", (uint8_t)MSG);
-    memcpy(server_buffer+MSG_TYPE_SIZE, cipher, TC_AES_BLOCK_SIZE + 1);
-    
-    // Send encrypted message
-    udp_send(argv[1], DEFAULT_PORT, server_buffer, TC_AES_BLOCK_SIZE + 1 + MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
-
     return 0;
 }
--- public-key-exchange/udp.h
+++ public-key-exchange/udp.h
@@ -1,67 +1,18 @@
 /*
-*   @ingroup my_examples
-*   @{
-*       @file       udp.h
-*       @brief      udp header file 
-*       @author     Stefano Milani <stefano.milani96@gmail.com>
-*   }@
+*  
+*	@file       udp.h
+*	@brief      udp header file 
+*	@author     Stefano Milani <stefano.milani96@gmail.com>
+*   
 */
 
 #ifndef UDP_H
 #define UDP_H
 
-/* needed for posix usleep */
-#ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE 600
-#endif
-
-#ifndef DEFAULT_NUM
-#define DEFAULT_NUM 1
-#endif
-#ifndef DEFAULT_DELAY
-#define DEFAULT_DELAY 1000000
-#endif
-// Server utils define
-#define SERVER_MSG_QUEUE_SIZE   (8)
-#define SERVER_BUFFER_SIZE      (128)
-#define DEFAULT_PORT            "8043"
-// Message utils define 
-#define MSG_TYPE_SIZE   1
-#define KEY_REQUEST     1
-#define KEY_ACK         2
-#define ACK             3
-#define MSG             4
-
-
-#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-// Network libraries 
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
-// Thread library
-#include "thread.h"
-// Support functions
-#include "utils.h"
-// AES library
-#include "tinycrypt/aes.h"
 
-
-// Start server thread function
-void *_server_thread(void *args);
-
-// Handle received messages
-int handle_message(char* src_addr);
-
-// Store device info
-int store_dev_info(char* addr, uint8_t* compr);
-
-// Sent UDP packet
-int udp_send(char *addr_str, char *port_str, char *data, size_t data_len, 
-                unsigned int num, unsigned int delay);
+#define DEFAULT_PORT            "8043"
 
 // Start UDP server 
 int udp_start_server(char *port_str);
