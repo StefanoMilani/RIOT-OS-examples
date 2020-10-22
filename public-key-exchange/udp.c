982429c26399e4c1ec80ef13943eab256da0f6fd
MilaniStef <stefano.milani96@gmail.com>
2020-10-22 18:36:42 +0200

    Removed tinycrypt library
    The decryption and encryption of messages is handled relying on the
    internal Crypto module of RIOT-OS. Tinycrypt library removed completely.
    
    To ensure the correct execution uncomment line number 37 of the file
    sys/include/crypto/ciphers.h (#define CRYPTO_AES) starting from the RIOT
    base directory. This enables the CRYPTO_AES module when compiling.
    
    Crypto module: https://api.riot-os.org/group__sys__crypto.html
    
    Signed-Off-By: Stefano Milani <stefano.milani96@gmail.com>

--- public-key-exchange/udp.c
+++ public-key-exchange/udp.c
@@ -35,6 +35,7 @@
 #define DEFAULT_DELAY 1000000
 #endif
 
+
 #include <unistd.h>
 #include <string.h>
 #include <inttypes.h>
@@ -44,8 +45,7 @@
 
 #include "thread.h"
 #include "key-management.h"
-#include "tinycrypt/aes.h"
-#include "tinycrypt/constants.h"
+#include "crypto/aes.h"
 
 #include "udp.h"
 
@@ -75,9 +75,9 @@ Device* dev=NULL;
 
 // Function declarations
 int udp_send(char *addr_str, char *port_str, char *data, size_t data_len, unsigned int num, unsigned int delay);
-int handle_message(char* src_addr);
+int handle_message(char* src_addr, int msg_size);
 int manage_key_request(char* src_addr);
-int decrypt_received_message(char* src_addr);
+int decrypt_received_message(char* src_addr, int msg_size);
 int store_dev_info(char* addr, uint8_t* compr);
 void *_server_thread(void *args);
 int send_ack(char* src_addr);
@@ -120,7 +120,7 @@ int udp_send(char *addr_str, char *port_str, char *data, size_t data_len,  unsig
 }
 
 // Handle received message
-int handle_message(char* src_addr) {
+int handle_message(char* src_addr, int msg_size) {
     uint8_t msg_type;
     // Get message type from message (First char)
     memcpy(&msg_type, server_buffer, MSG_TYPE_SIZE);
@@ -146,8 +146,8 @@ int handle_message(char* src_addr) {
             break;
         case MSG:
 			// Ecrypted message received
-			if(decrypt_received_message(src_addr) == -1) {
-				perror("Failed to decrypt received message");
+			if(decrypt_received_message(src_addr, msg_size) == -1) {
+				perror("Failed to  received message");
 				return -1;
 			}
             break;
@@ -221,18 +221,20 @@ int send_ack(char* src_addr) {
 }
 
 // Encrypted message received
-int decrypt_received_message(char* src_addr) {
-    printf("\n-Decrypting message!\n");
+int decrypt_received_message(char* src_addr, int msg_size) {
+    printf("\n-Decrypting message! Size: %d\n", msg_size);
     // Initializing aes struct
-    struct tc_aes_key_sched_struct s;
-    // Set key
-    if(tc_aes128_set_decrypt_key(&s, (const uint8_t *)dev->secret) == TC_CRYPTO_FAIL) {
-		perror("Failed to set aes decrypt key");
+    cipher_t aes_context;
+    // Initialize AES context 
+    if(cipher_init(&aes_context, CIPHER_AES_128, (const uint8_t*)dev->secret, AES_KEY_SIZE) != CIPHER_INIT_SUCCESS ) {
+		perror("Failed to initialize aes context");
 		return -1;
-	} 
-    // Decrypt
-    uint8_t msg[TC_AES_BLOCK_SIZE + 1];
-    if(tc_aes_decrypt(msg, (const uint8_t*) server_buffer+MSG_TYPE_SIZE, &s) == TC_CRYPTO_FAIL) {
+	}
+    // Decrypt 
+    uint8_t msg[AES_BLOCK_SIZE];
+	memset(&msg, 0x00, AES_BLOCK_SIZE);
+	if(cipher_decrypt(	(const cipher_t*) &aes_context, 
+ 			   			(const uint8_t*) server_buffer+MSG_TYPE_SIZE, msg)	< 0 ) {
 		perror("Failed to decrypt message");
 		return -1;
 	}
@@ -309,21 +311,26 @@ int send_encrypted(int argc, char **argv){
         return -1;
     }
     // Initialize aes struct
-    struct tc_aes_key_sched_struct s;
+    cipher_t aes_context;
 
-    // Initialize key
-    tc_aes128_set_encrypt_key(&s, dev->secret);
+    // Initialize context
 
-    // Encrypt data
-    uint8_t cipher[TC_AES_BLOCK_SIZE + 1];
-    tc_aes_encrypt(cipher, (const uint8_t *)argv[2], &s);
+    int ret = cipher_init(&aes_context, CIPHER_AES_128,  (const uint8_t*) dev->secret, AES_KEY_SIZE);
+	if(ret != CIPHER_INIT_SUCCESS) {
+		printf("ERROR: %d\n", ret);
+		perror("Failed to initialize aes context");
+		return -1;
+	}
+
+    // Encrypt message, argv[2] contains the message to be encrypted
+    memset(server_buffer, 0x00, SERVER_BUFFER_SIZE);
+    cipher_encrypt((const cipher_t*)&aes_context, (const uint8_t *) argv[2], (uint8_t*) server_buffer+MSG_TYPE_SIZE);
 
     // Forge server buffer
 	memset(server_buffer, MSG, MSG_TYPE_SIZE);
-    memcpy(server_buffer+MSG_TYPE_SIZE, cipher, TC_AES_BLOCK_SIZE + 1);
     
     // Send encrypted message
-    udp_send(argv[1], DEFAULT_PORT, server_buffer, TC_AES_BLOCK_SIZE + 1 + MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
+    udp_send(argv[1], DEFAULT_PORT, server_buffer, AES_BLOCK_SIZE + MSG_TYPE_SIZE, DEFAULT_NUM, DEFAULT_DELAY);
 
     return 0;
 }
@@ -362,15 +369,15 @@ void *_server_thread(void *args) {
     }
     printf("Success: started UDP server on port %" PRIu16 "\n", port);
     while (1) {
-        int res;
+        int ret;
         struct sockaddr_in6 src;
         socklen_t src_len = sizeof(struct sockaddr_in6);
         memset(server_buffer, 0, SERVER_BUFFER_SIZE);
-        if ((res = recvfrom(server_socket, server_buffer, sizeof(server_buffer), 0,
+        if ((ret = recvfrom(server_socket, server_buffer, sizeof(server_buffer), 0,
                             (struct sockaddr *)&src, &src_len)) < 0) {
             puts("Error on receive");
         }
-        else if (res == 0) {
+        else if (ret == 0) {
             puts("Peer did shut down");
         }
         else {
@@ -379,7 +386,7 @@ void *_server_thread(void *args) {
                 printf("Failed to parse IP address");
                 continue;
             }
-            handle_message(src_addr);
+            handle_message(src_addr, ret);
         }
     }
     return NULL;
